d24d58b9a7c443906870fc045bea0a521fdb1b15
juanozorio <juan_ozorio@outlook.com>
2021-08-22 13:03:43 -0300

    Usando Ajax

--- front-end/public/src/components/cadastro/cadastro.js
+++ front-end/public/src/components/cadastro/cadastro.js
@@ -1,14 +1,20 @@
-/*function validate() {
-    event.preventDefault();
-
-    var objCadastro = {
-        nome: document.getElementById('nome').value,
-        email: document.getElementById('email').value,
-        celular: document.getElementById('celular').value,
-        senha: document.getElementById('senha').value
-    };
-    console.log(objCadastro);
-
-    //Importar a funcao da Action
-        
-    // Chamar a funcao importada e passar os quatros valores como parametros*/
+const objCadastro = {
+    'nome': document.getElementById('nome').value,
+    'email': document.getElementById('email').value,
+    'celular': document.getElementById('celular').value,
+    'senha': document.getElementById('senha').value,
+  }
+  
+  var dados = JSON.stringify(objCadastro);
+  /*
+  $.ajax({
+    url: 'recebe.php',
+    type: 'POST',
+    data: {data: dados},
+    success: function(result){
+      // Retorno se tudo ocorreu normalmente
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // Retorno caso algum erro ocorra
+    }
+  });*/
