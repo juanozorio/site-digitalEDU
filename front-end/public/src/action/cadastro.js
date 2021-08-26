e4c3d22e64ac5d80f19752993db47f2521c5fe20
isac.chuab <isac.chuab@eduzz.com>
2021-08-26 15:44:46 -0300

    fazendo processo de código

--- front-end/public/src/action/cadastro.js
+++ front-end/public/src/action/cadastro.js
@@ -1,8 +1,10 @@
 //Exportar uma funcao que recebe os quatros valores
 
+export default function teste(objCadastro) {
+  console.log(objCadastro);
+    return alert('aqui dentro da função');
 
-export const teste = (d1, d2, d3, d4) => async () => {
-    console.log("aqui")
+    // return console.log(d1, d2, d3, d4);
     /*try {
       const response = await VOU CHAMAR MINHA FUNCAO DO PHP AQUI
 
--- front-end/public/src/components/cadastro/cadastro.js
+++ front-end/public/src/components/cadastro/cadastro.js
@@ -1,22 +1,20 @@
-import { teste } from "../../action/cadastro.js";
+import teste from "../../action/cadastro.js";
 
 window.onload = function () {
-    
     const botaoEnviar = document.getElementById('button-cadastro');
+
     botaoEnviar.addEventListener("click", async dados => {
-        console.log(dados);
-       await teste("d1", "d2", "d3", "d4");
+       await pegarDados();
     })
-}   
-/*
+}
+
 function pegarDados() {
-    console.log("inicio aq")
     var objCadastro = {
         'nome': document.getElementById('nome').value,
         'email': document.getElementById('email').value,
         'celular': document.getElementById('celular').value,
         'senha': document.getElementById('senha').value,
     }
-    console.log(objCadastro);
+
+    teste(objCadastro);
 }
-*/
